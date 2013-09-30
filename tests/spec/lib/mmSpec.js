96be06c4a41e4a84f110d06e022b253ab9e6e3c1
Dominic Orme <dominic.orme@kineo.com>
2013-09-30 16:43:51 +0000

    Tests for mm.js Spelling and commenting fixes in app.js Additional functions for ease of testing in mm.js SpecRunner.html to include the matcher.sequential.js file matcher.sequential.js as an additional testing method for Jasmine mmSpec.js as the test suite.

--- tests/spec/lib/mmSpec.js
+++ tests/spec/lib/mmSpec.js
@@ -1,92 +1,315 @@
 describe("MM", function() {
 
+    function resetMM() {
+        // Values on the MM object
+        config = {};
+        plugins = [];
+        models = {};
+        collections = {};
+        deviceType = 'phone';
+        clickType = 'click';
+        quickClick = 'click';
+        deviceReady = false;
+        deviceOS = '';
+        logData = [];
+        inComputer = false;
+        touchMoving = false;
+        scrollType = '';
+        mq = 'only screen and (min-width = 768px) and (-webkit-min-device-pixel-ratio = 1)';
+
+        if (MM.backup !== undefined) {
+            // Bolt-ons to the MM object
+            MM.util = _.clone(MM.backup.util);
+            MM.lang = _.clone(MM.backup.lang);
+            MM.mq = _.clone(MM.backup.mq);
+            MM.config = _.clone(MM.backup.config);
+        } else {
+            MM.backup = {};
+        }
+    }
+
+    beforeEach(function() {
+        resetMM();
+
+        var config = {
+            'hello' : 'world'
+        };
+        var lang = JSON.stringify({
+            'astring':'some phrase to return'
+        });
+        MM.init(config);
+        MM.lang.base = JSON.parse(lang);
+        MM.backup.util = _.clone(MM.util);
+        MM.backup.lang = _.clone(MM.lang);
+        MM.backup.mq = _.clone(MM.mq);
+        MM.backup.config = _.clone(MM.config);
+    });
+
     describe("init", function() {
-        it("sets MM.config", function() {
+        describe("MM.config has been set", function() {
+            it("has the correct config", function() {
+                expect(MM.config).toEqual({'hello':'world'});
+            });
+        });
+        describe("sets correct event type", function() {
+            // mock MM.util.isTouchDevice to return true
+            // test MM.clickType = 'touchend'
+            // test MM.quickClick = 'touchstart'
+            it("when we have a touch device", function() {
+                MM.util.isTouchDevice = spyOn(MM.util, "isTouchDevice").andReturn(true);
+                MM.init({});
+                expect(MM.util.isTouchDevice).toHaveBeenCalled();
+                expect(MM.clickType).toEqual('touchend');
+                expect(MM.quickClick).toEqual('touchstart');
+            });
+
+            // mock MM.util.isTouchDevice to return false
+            // test MM.clickType = 'click'
+            // test MM.quickClick = 'click'
+            it("when we don't have a touch device", function() {
+                spyOn(MM.util, "isTouchDevice").andReturn(false);
+                MM.init({});
+                expect(MM.util.isTouchDevice).toHaveBeenCalled();
+                expect(MM.clickType).toEqual('click');
+                expect(MM.quickClick).toEqual('click');
+            });
+
         });
 
-        it("calls setTouchHandlers", function() {
-        });
+        describe("calls setDeviceType", function() {
+            it("Sets the device to a tablet", function() {
+                var matchMediaResponse = {
+                    matches: true
+                };
+                MM.mq = {'hello':'world'};
+                spyOn(window, "matchMedia").andReturn(matchMediaResponse);
+                MM.init({});
+                expect(window.matchMedia).toHaveBeenCalledWith({'hello':'world'});
+                expect(MM.deviceType).toEqual('tablet');
+                expect($("body").hasClass('tablet')).toEqual(true);
+            });
 
-        it("calls setDeviceType", function() {
-        });
-
-        it("calls setDeviceOS", function() {
-        });
-
-        it("calls setInComputerState", function() {
-        });
-
-        it("calls loadCordova", function() {
-        });
-
-        it("calls loadBackboneRoater", function() {
-        });
-
-        it("calls setUpAjaxErrorHandling", function() {
-        });
-
-        it("calls loadModels", function() {
-        });
-
-        it("calls loadSettings", function() {
-        });
-
-        describe("setTouchHandlers", function() {
-            it("sets properties correctly", function() {
+            it("Sets the device to a phone", function() {
+                var matchMediaResponse = {
+                    matches: false
+                };                
+                MM.mq = {'hello':'world'};
+                spyOn(window, "matchMedia").andReturn(matchMediaResponse);
+                MM.init({});
+                expect(window.matchMedia).toHaveBeenCalledWith({'hello':'world'});
+                expect(MM.deviceType).toEqual('phone');
+                expect($("body").hasClass('phone')).toEqual(true);
             });
         });
 
-        describe("setDeviceType", function() {
-            it("sets properties correctly", function() {
+        describe("calls setDeviceOS", function() {
+            // Mock navigator.userAgent to be the string iPhone
+            // test this.deviceOS is ios
+            it("sets OS to ios when iPhone in userAgent", function() {
+                spyOn(MM, "_getUserAgent").andReturn("12345 iphone abcdef");
+                MM.init({});
+                expect(MM.deviceOS).toEqual('ios');
             });
+
+            // Mock navigator.userAgent to be the string iPad
+            // test this.deviceOS is ios
+            it("sets OS to ios when iPad in userAgent", function() {
+                spyOn(MM, "_getUserAgent").andReturn("12345 ipad abcdef");
+                MM.init({});
+                expect(MM.deviceOS).toEqual('ios');
+            });            
+
+            // Mock navigator.userAgent to be the string Android
+            // test this.deviceOS is android
+            it("sets OS to android when Android in userAgent", function() {
+                spyOn(MM, "_getUserAgent").andReturn("12345 android abcdef");
+                MM.init({});
+                expect(MM.deviceOS).toEqual('android');
+            });            
+
+            // Mock navigator.userAgent to be the string foo
+            // test this.deviceOS is 'null'
+            it("sets OS to null when garbage in userAgent", function() {
+                spyOn(MM, "_getUserAgent").andReturn("12345 some other user agent abcdef");
+                MM.init({});
+                expect(MM.deviceOS).toEqual('null');
+            });            
+
+            // Mock navigator.userAgent to be the string iPhone Android
+            // test this.deviceOS is ios
+            it("sets OS to ios when userAgent is contains both iPhone and Android", function() {
+                spyOn(MM, "_getUserAgent").andReturn("12345 android iphone abcdef");
+                MM.init({});
+                expect(MM.deviceOS).toEqual('ios');
+            });            
         });
 
-        describe("setDeviceOS", function() {
-            it("sets properties correctly", function() {
-            });
+        it("calls loadBackboneRouter", function() {
+            var myBackboneRouter = function() {
+                var obj = {};
+                obj.route = function() {};
+                obj.name = 'An anonymous router';
+                return obj;
+            };
+            spyOn(Backbone.Router, 'extend').andReturn(myBackboneRouter);
+            MM.init({});
+            expect(Backbone.Router.extend).toHaveBeenCalled();
+            expect(MM.Router.name).toEqual("An anonymous router");
+            // Mock Backbone.Router.extend to return a fake object
+            // test this.Router is an instance of the fake object
         });
 
-        describe("setInComputerState", function() {
-            it("sets properties correctly", function() {
-            });
+        it("sets ajax defaults", function() {
+            spyOn($, 'ajaxSetup').andCallThrough();
+            MM.init({});
+            expect($.ajaxSetup).toHaveBeenCalled();
         });
 
-        describe("loadCordova", function() {
-            it("sets properties correctly", function() {
-            });
+        it("loads core models", function() {
+            var modelsArray = [
+                'setting','site','course','user','cacheEl','syncEl'
+            ];
+            var collectionsArray = [
+                'settings', 'sites', 'courses', 'users', 'cache', 'sync'
+            ];
+            MM.init({});
+            expect(_.keys(MM.models)).toEqual(modelsArray);
+            expect(_.keys(MM.collections)).toEqual(collectionsArray);
         });
 
-        describe("loadBackboneRouter", function() {
-            it("sets properties correctly", function() {
-            });
-        });
+        it("loads routes", function() {
+            // We use our Router for this test.
+            var myBackboneRouter = function() {
+                var obj = {};
+                obj.route = function(a, b, c) {};
+                obj.name = 'An anonymous router';
+                return obj;
+            };
 
-        describe("setUpAjaxErrorHandling", function() {
-            it("sets properties correctly", function() {
-            });
-        });
+            // We don't want the Router doing anything in the background
+            // so use our harmless one.            
+            spyOn(Backbone.Router, 'extend').andReturn(myBackboneRouter);
 
-        describe("loadModels", function() {
-            it("sets properties correctly", function() {
-            });
-        });
+            MM.util.helpMeLogin = 'Help Me Login Text';
 
-        describe("loadSettings", function() {
-            it("sets properties correctly", function() {
-            });
+            MM.settings = {
+                'display' : 'Settings display',
+                'showSection': 'Settings show Section',
+                'showSite':'Settings show site',
+                'addSite':'Settings add site',
+                'deleteSite':'Settings delete site'
+            };
+
+            MM.cache = {
+                'purge' : 'Purge Cache Function'
+            };
+
+            MM.refresh = 'Some refresh function';
+            MM.showLog = 'Some logging function';
+
+            var MMSyncLangFunction = function() {
+                MM.lang.sync(true);
+            };
+
+            var MMSyncCSSFunction = function() {
+                MM.sync.css(true);
+            };
+
+            MM.init({});
+
+            // Overwrite the currently instantiated Router
+            MM.Router = new myBackboneRouter();
+
+            // Set the spy on our new router
+            spyOn(MM.Router, 'route').andCallThrough();
+
+            // Recall the function we need to test.
+            MM.loadRoutes();
+
+            expect(MM.Router.route).toHaveBeenCalledSequentiallyWith(
+                [
+                    ['helpmelogin', 'helpmelogin', "Help Me Login Text"],
+                    ['settings', 'settings', 'Settings display'],
+                    ['refresh', 'refresh', 'Some refresh function'],
+                    ['settings/:section/', 'settings_section', 'Settings show Section'],
+                    ['settings/sites/:siteid', 'settings_sites_show_site', 'Settings show site'],
+                    ['settings/sites/add', 'settings_sites_add_site', 'Settings add site'],
+                    ['settings/sites/delete/:siteid', 'settings_sites_delete_site', 'Settings delete site'],
+                    ['settings/general/purgecaches', 'settings_general_purgecaches', 'Purge Cache Function'],
+                    ['settings/sync/lang', 'settings_sync_lang', MMSyncLangFunction],
+                    ['settings/sync/css', 'settings_sync_css', MMSyncCSSFunction],
+                    ['settings/development/log/:filter', 'settings_sync_css', 'Some logging function']
+                ]
+            );
+            expect(MM.Router.route.callCount).toBe(11);
         });
     });
 
     describe("deviceConnected", function() {
         it("reports state correctly when connected", function() {
+            var myTestNetwork = {
+                connection: {
+                    type: 'testnetwork type'
+                }
+            };
+
+            window.Connection = {
+                'NONE':'no connection',
+                'UNKNOWN':'unknown connection'
+            };
+
             // Mock network states and test connection is reported correctly 
+            spyOn(MM, 'deviceConnected').andCallThrough();
+            spyOn(MM, '_getNetwork').andCallFake(function() {
+                return myTestNetwork;
+            });
+            spyOn(MM, 'getConfig').andCallFake(function() {
+                return;
+            });
+            spyOn(MM, 'log').andCallThrough();
+            MM.init({});
+            var result = MM.deviceConnected();
+            expect(MM.deviceConnected).toHaveBeenCalled();
+            expect(result).toEqual(true);
+            expect(MM.log).toHaveBeenCalledSequentiallyWith(
+                [
+                    ['Initializating app'],
+                    ['Internet connection checked ' + true]
+                ]
+            );            
         });
 
         it("reports state correctly when disconnected", function() {
+            // Required by the test.
+            $(document.body).append(
+                $("<div>").attr('id', 'main-wrapper').css('display', 'block')
+            );
+
             // Mock network states and test connection is reported correctly 
+            spyOn(MM, 'deviceConnected').andCallThrough();
+            spyOn(MM, '_getNetwork').andCallFake(function() {
+                return;
+            });
+            spyOn(MM, 'getConfig').andCallFake(function() {
+                return true;
+            });
+            spyOn(MM, 'log').andCallThrough();
+            MM.init({});
+            var result = MM.deviceConnected();
+            expect(MM.deviceConnected).toHaveBeenCalled();
+            expect(result).toEqual(false);
+            expect(MM.log).toHaveBeenCalledSequentiallyWith(
+                [
+                    ['Initializating app'],
+                    ['Returning not connected (forced by settings)']
+                ]
+            );
+
+            // Remove the DOM element
+            $("#main-wrapper").remove();
         });
     });
-
+/*
     describe("loadLayout", function() {
         it("starts backbone history", function() {
         });
@@ -528,4 +751,5 @@ describe("MM", function() {
         it("reloads the site", function() {
         });
     });
+*/    
 });
