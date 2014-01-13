1679c05a044c6b734881746768059c0db2638334
Juan Leyva <juanleyvadelgado@gmail.com>
2014-01-13 17:28:04 +0100

    MOBILE-481 Support for taking photos using webcam

--- plugins/upload/main.js
+++ plugins/upload/main.js
@@ -14,23 +14,23 @@ define(function () {
             },
             toogler: true
         },
-        
+
         routes: [
             ["upload/browse", "upload_browse", "browseAlbums"],
             ["upload/take", "upload_take", "takeMedia"],
             ["upload/record", "upload_record", "recordAudio"],
         ],
-        
+
         browseAlbums: function() {
             MM.log('Trying to get a image from albums', 'Upload');
             MM.Router.navigate("");
-            
+
             var width  =  $(document).innerWidth()  - 200;
             var height =  $(document).innerHeight() - 200;
-            
+
             // iPad popOver, see https://tracker.moodle.org/browse/MOBILE-208
             var popover = new CameraPopoverOptions(10, 10, width, height, Camera.PopoverArrowDirection.ARROW_ANY);
-            
+
             navigator.camera.getPicture(MM.plugins.upload.photoSuccess, MM.plugins.upload.photoFails, {
                 quality: 50,
                 destinationType: navigator.camera.DestinationType.FILE_URI,
@@ -48,13 +48,13 @@ define(function () {
                 destinationType: navigator.camera.DestinationType.FILE_URI
             });
         },
-        
+
         recordAudio: function() {
             MM.Router.navigate("");
             MM.log('Trying to record and Audio', 'Upload');
             navigator.device.capture.captureAudio(MM.plugins.upload.recordAudioSuccess, MM.plugins.upload.recordAudioFails, {limit: 1});
         },
-        
+
         photoSuccess: function(uri) {
 
             MM.log('Uploading an image to Moodle', 'Upload');
@@ -62,34 +62,41 @@ define(function () {
 
             var options = {};
             options.fileKey="file";
-            options.fileName="image_"+d.getTime()+".jpg";
+
+            // Check if is a URI or a file system path.
+            if (uri.indexOf('data:') > -1) {
+                options.fileName="image_"+d.getTime()+".jpg";
+            } else {
+                options.fileName= uri.lastIndexOf("/") + 1;
+            }
+
             options.mimeType="image/jpeg";
-            
+
             MM.moodleUploadFile(uri, options,
                                 function(){ MM.popMessage(MM.lang.s("imagestored")); },
                                 function(){ MM.popErrorMessage(MM.lang.s("erroruploading")) }
-            );       
-                       
+            );
+
         },
-        
+
         photoFails: function(message) {
             MM.log('Error trying getting a photo', 'Upload');
             if (message.toLowerCase().indexOf("error") > -1 || message.toLowerCase().indexOf("unable") > -1) {
                 MM.popErrorMessage(message);
             }
         },
-        
+
         recordAudioSuccess: function(mediaFiles) {
-            
+
             MM.log('Auddio sucesfully recorded', 'Upload');
-            
+
             var i, len;
             for (i = 0, len = mediaFiles.length; i < len; i += 1) {
                 var options = {};
                 options.fileKey = null;
                 options.fileName = mediaFiles[i].name;
                 options.mimeType = null;
-                
+
                 MM.moodleUploadFile(mediaFiles[i].fullPath, options,
                                     function(){
                                         MM.popMessage(MM.lang.s("recordstored"));
@@ -97,22 +104,22 @@ define(function () {
                                     function(){
                                         MM.popErrorMessage(MM.lang.s("erroruploading"))
                                     }
-                ); 
-            } 
+                );
+            }
         },
-        
+
         recordAudioFails: function(error) {
             if (!error) {
                 error = { code: 0};
             }
-            
+
             MM.log('Error trying recording an audio ' + error.code, 'Upload');
             if (error.code != CaptureError.CAPTURE_NO_MEDIA_FILES) {
                 MM.popErrorMessage(MM.lang.s("errorcapturingaudio"));
             }
         }
     }
-    
+
     MM.registerPlugin(plugin);
 
 });
